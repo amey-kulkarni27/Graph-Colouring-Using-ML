2989f3c8cc0589bacee1d5887fa1057543820918
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-09-17 00:36:49 +0530

    main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from generate_kpart import gen_kpart
 from feature_vector import feature_vector
 
+num_graphs = 5
 k = 3
 n = 4
 p = 0.4
-G = gen_kpart(k, n, p)
-vec = feature_vector(G)
+G_list = [gen_kpart(k, n, p) for i in range(num_graphs)]
+for G in G_list:
+    vec = feature_vector(G)
