985f2dbcdc1f4684e7f4712cde1d13fd742f61a5
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-09-17 02:15:41 +0530

    Architecture/loss function to be decided

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from generate_kpart import gen_kpart
+from operations import operations, vertex_pair, vertex_pair_opt, get_label
+from generate_kpart import gen_kpart, display_graph
 from feature_vector import feature_vector
+from operations import operations
+import random
 
-num_graphs = 5
+
+num_graphs = 1
 k = 3
 n = 4
 p = 0.4
 G_list = [gen_kpart(k, n, p) for i in range(num_graphs)]
-for G in G_list:
+for G, coords in G_list:
     vec = feature_vector(G)
+    merge = random.uniform(0, 1) < 0.5
+    # nodes = vertex_pair(G, n * k)
+    nodes = vertex_pair_opt(G)
+    label = get_label(n, merge, nodes)
+    print(merge, nodes, label)
+    display_graph(G, coords)
+    G = operations(G, merge, nodes)
+    display_graph(G, coords)
