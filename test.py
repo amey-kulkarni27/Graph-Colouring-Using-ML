e5b0c539e0af1bcb7297a207325a2f046d726709
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-11-05 03:28:36 +0530

    Timed

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+from operator import concat
 import numpy as np
+from scipy.sparse import coo
 from operations import operations, vertex_pair_opt, vertex_pair_non_edge
 from feature_vector import feature_vector
 from generate_kpart import display_graph
 import networkx as nx
 import copy
+from timeit import default_timer as timer
 
 def correctness(G_init, G):
     for u in G.nodes():
@@ -30,22 +33,44 @@ def test(G, coords, clf, n, k, interval=1, method="top_k"):
     G_init = copy.deepcopy(G)
     # display_graph(G, coords)
     cnt = 0
+    steps, update_steps = 0, 0
+    fvt, nt, concatt, at, mpt = 0, 0, 0, 0, 0
     while (vertex_pair_non_edge(G)) != False:
         if cnt == 0:
+            t1 = timer()
             vec = feature_vector(G, method=method, k=k)
+            t2 = timer()
+            update_steps += 1
+            fvt += t2 - t1
 
+        t3 = timer()
         nodes = vertex_pair_non_edge(G)
+        t4 = timer()
         x = np.concatenate((vec[nodes[0]], vec[nodes[1]]))
         x = x.reshape(1, -1)
         action = clf.predict(x)
+        t5 = timer()
         G = operations(G, action, nodes)
         # display_graph(G, coords)
         cnt += 1
         cnt %= interval
         N = len(G.nodes)
+        t6 = timer()
         mapping = {old: new for (old, new) in zip(G.nodes, [i for i in range(N)])}
         G = nx.relabel_nodes(G, mapping)
-    display_graph(G, coords)
+        t7 = timer()
+        steps += 1
+        nt += t4 - t3
+        at += t5 - t4
+        mpt += t7 - t6
+    print("Steps Taken:", steps)
+    print("Feature Vector:", round(fvt, 2))
+    print("Nodes:", round(nt, 2))
+    print("Concatenation:", round(concatt, 2))
+    print("Predict:", round(at, 2))
+    print("Remapping:", round(mpt, 2))
+    print()
+    # display_graph(G, coords)
 
     # correctness(G_init, G)
     return G
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from operations import operations, vertex_pair, vertex_pair_opt, vertex_pair_non
 from feature_vector import feature_vector
 from generate_kpart import display_graph
 import networkx as nx
+from timeit import default_timer as timer
 
 def train(G, coords, X, y, n, k, interval=1, method="top_k"):
     '''
@@ -17,14 +18,27 @@ def train(G, coords, X, y, n, k, interval=1, method="top_k"):
     '''
     # display_graph(G, coords)
     cnt = 0
-    z=0
-    while (vertex_pair_non_edge(G)) != False:
+    steps, update_steps = 0, 0
+    condt, fvt, nt, concatt, at, mpt = 0, 0, 0, 0, 0, 0
+    # while (vertex_pair_non_edge(G)) != False:
+    while True:
+        t01 = timer()
+        if (vertex_pair_non_edge(G)) == False:
+            break
+        t02 = timer()
         if cnt == 0:
+            t1 = timer()
             vec = feature_vector(G, method=method, k=k)
+            update_steps += 1
+            t2 = timer()
+            fvt += t2 - t1
 
         # nodes = vertex_pair(G, n * k)
+        t3 = timer()
         nodes = vertex_pair_non_edge(G)
+        t4 = timer()
         action = get_action(G, nodes)
+        t5 = timer()
         x = np.concatenate((vec[nodes[0]], vec[nodes[1]]))
         X.append(x)
         y.append(action)
@@ -32,7 +46,21 @@ def train(G, coords, X, y, n, k, interval=1, method="top_k"):
         # display_graph(G, coords)
         cnt += 1
         cnt %= interval
-        z+=1
+        steps+=1
         N = len(G.nodes)
+        t6 = timer()
         mapping = {old: new for (old, new) in zip(G.nodes, [i for i in range(N)])}
         G = nx.relabel_nodes(G, mapping)
+        t7 = timer()
+        condt += t02 - t01
+        nt += t4 - t3
+        at += t5 - t4
+        concatt += t6 - t5
+        mpt += t7 - t6
+    print("Steps Taken:", steps)
+    print("Condition Checking:", round(condt, 2))
+    print("Feature Vector:", round(fvt, 2))
+    print("Nodes:", round(nt, 2))
+    print("Concatenation:", round(concatt, 2))
+    print("Action:", round(at, 2))
+    print("Remapping:", round(mpt, 2))
