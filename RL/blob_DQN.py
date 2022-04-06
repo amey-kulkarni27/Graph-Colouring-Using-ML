ce7f00176ba5ccbafe6175b9abaeefbea6615943
amey-kulkarni27 <amey2704@gmail.com>
2022-04-07 00:09:11 +0530

    blob DQN

--- RL/blob_DQN.py
+++ RL/blob_DQN.py
@@ -15,7 +15,7 @@ import cv2
 
 
 DISCOUNT = 0.99
-REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
+REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training (basically batch size)
 MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
 MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
 UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
@@ -24,7 +24,7 @@ MIN_REWARD = -200  # For model save
 MEMORY_FRACTION = 0.20
 
 # Environment settings
-EPISODES = 20_000
+EPISODES = 2_000
 
 # Exploration settings
 epsilon = 1  # not a constant, going to be decayed
